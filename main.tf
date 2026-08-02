de06ae4c4e33555b1b12bf90205bc24d7dae3037
nssrmhmmd1 <153364732+nssrmhmmd1@users.noreply.github.com>
2026-08-02 11:20:05 +0300

    Update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "qa" {
 source = "./modules/blog"
-Project_ID = var.Project_ID
+project_ID = var.project_ID
 region = var.region
 app_name = "qa-blog"
 Network_name = "qa"
@@ -10,7 +10,7 @@ Network_name = "qa"
 module "staging" {
 
 source = "./modules/blog"
-Project_ID = var.Project_ID
+project_ID = var.project_ID
 region = var.region
 app_name = "staging-blog"
 Network_name = "staging"
@@ -20,7 +20,7 @@ Network_name = "staging"
 module "Prod" {
     
 source = "./modules/blog"
-Project_ID = var.Project_ID
+project_ID = var.project_ID
 region = var.region
 app_name = "Prod-blog"
 Network_name = "Prod"
