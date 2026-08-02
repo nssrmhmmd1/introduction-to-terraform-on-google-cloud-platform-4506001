216fb8efa3e876b4c8150587148c5fb7dc44ff43
nssrmhmmd1 <153364732+nssrmhmmd1@users.noreply.github.com>
2026-08-02 12:38:47 +0300

    Web Services

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -1,6 +1,25 @@
+resource "google_compute_network" "appnet2" {
+  name                    = var.network_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "appnet2" {
+  name          = "${var.network_name}-subnet"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = var.region
+  network       = google_compute_network.appnet2.id
+}
+
+data "google_compute_image" "ubuntu" {
+  most_recent = true
+  project     = "ubuntu-os-cloud"
+  family      = "ubuntu-2204-lts"
+}
+
 resource "google_compute_instance" "web" {
   name         = var.app_name
   machine_type = "e2-micro"
+  zone         = "${var.region}-a"
 
   metadata_startup_script = <<-EOT
     #!/bin/bash
@@ -25,6 +44,3 @@ resource "google_compute_instance" "web" {
     access_config {}
   }
 }
-
-
-
