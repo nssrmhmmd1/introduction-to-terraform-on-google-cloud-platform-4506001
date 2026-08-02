97de3c655d08ce4a197b91a6bcbce070cf04639e
nssrmhmmd1 <153364732+nssrmhmmd1@users.noreply.github.com>
2026-08-02 10:15:03 +0300

    Staging and Prod

--- main.tf
+++ main.tf
@@ -1,8 +1,28 @@
-module "qa"
-{
+module "qa" {
 source = "./modules/blog"
-
+Project_ID = var.Project_ID
+region = var.region
 app_name = "qa-blog"
 Network_name = "qa"
+
+}
+
+module "staging" {
+
+source = "./modules/blog"
+Project_ID = var.Project_ID
+region = var.region
+app_name = "staging-blog"
+Network_name = "staging"
+
+}
+
+module "Prod" {
+    
+source = "./modules/blog"
+Project_ID = var.Project_ID
+region = var.region
+app_name = "Prod-blog"
+Network_name = "Prod"
 
 }
--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output  "qa-ip" {
 
     value = module.qa.public_ip
 }
+
+output  "staging-ip" {
+
+    value = module.staging.public_ip
+}
+
+output  "Prod-ip" {
+
+    value = module.Prod.public_ip
+}
